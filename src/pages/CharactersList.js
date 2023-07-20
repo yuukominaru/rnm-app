24acb744f37839b1e69949d14b0450737a568d08
yuukominaru <patriciaaela@gmail.com>
2023-07-20 20:10:40 +0700

    pretty-fy ui

--- src/pages/CharactersList.js
+++ src/pages/CharactersList.js
@@ -1,36 +1,34 @@
 import React from "react";
-import { useQuery, gql } from "@apollo/client";
-
-const GET_CHARACTERS = gql`
-    query {
-        characters {
-            results {
-                id
-                name
-                image
-            }
-        }
-    }
-`;
+import { useCharacters } from "../hooks/useCharacters";
+import { Card } from "../components/Card";
 
 export default function CharactersList() {
-    const { error, loading, data } = useQuery(GET_CHARACTERS);
+  const { error, loading, data } = useCharacters();
 
-    console.log({ error, loading, data });
+  console.log({ error, loading, data });
 
-    if (loading) return <div>Spinner</div>;
-    if (error) return <div>Something went wrong</div>;
-
+  if (loading)
     return (
-        <div className="CharacterList">
-            {data.characters.results.map((character) => {
-                return (
-                    <div>
-                        <h3>{character.name}</h3>
-                        <img src={character.image} />
-                    </div>
-                );
-            })}
+      <div className="container">
+        <div className="d-flex justify-content-center">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
         </div>
+      </div>
     );
+
+  if (error) return <div>Something went wrong</div>;
+
+  return (
+    <div className="CharacterList container justify-content-center">
+      <div className="row row-cols-4">
+        {data.characters.results.map((character) => {
+          return (
+            <Card character={character} key={character.id} />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
