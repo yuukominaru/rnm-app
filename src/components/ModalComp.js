e41bb7fd8e537b2837c192896e07a73e9f4a2188
yuukominaru <ela_patricia@yahoo.co.id>
2023-07-22 19:12:59 +0700

    changed modal form

--- src/components/ModalComp.js
+++ src/components/ModalComp.js
@@ -25,10 +25,6 @@ export function ModalComp(props) {
               autoFocus
             />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Example textarea</Form.Label>
-            <Form.Control as="textarea" rows={3} />
-          </Form.Group>
         </Form>
       </Modal.Body>
       <Modal.Footer>
