f395fdb767f7061a3dda2480db1e5674f6e73086
yuukominaru <ela_patricia@yahoo.co.id>
2023-07-22 19:13:34 +0700

    moved to react bootstrap

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import Spinner from "react-bootstrap/Spinner";
 import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { useCharacter } from "../hooks/useCharacter";
 import { useParams } from "react-router";
 import { ModalComp } from "../components/ModalComp";
@@ -36,52 +39,50 @@ export default function Character() {
     );
 
   return (
-    <div
-      className="Character container m-5 mx-auto row justify-content-center"
-      key={data.character.id}
-    >
-      <div className="col text-center">
-        <div className="row">
-          <img
-            src={data.character.image}
-            className="img-fluid"
-            alt={data.character.name}
-          />
-        </div>
-        <div className="row">
-          <Button variant="primary" onClick={handleShow}>
-            Add Location
-          </Button>
+    <>
+      <Container className="Character mt-3" key={data.character.id}>
+        <Row>
+          <Col sm md>
+            <Row>
+              <Image src={data.character.image} fluid />
+            </Row>
 
-          <ModalComp show={show} onHide={handleClose} />
-        </div>
-      </div>
-      <div className="col">
-        <h2>{data.character.name}</h2>
-        <div>
-          <b>Status:</b> {data.character.status}
-          <br></br>
-          <b>Species:</b> {data.character.species}
-          <br></br>
-          <b>Gender:</b> {data.character.gender}
-          <br></br>
-          <b>Origin:</b> {data.character.origin.name}
-          <br></br>
-          Episodes:
-          <ul>
-            {data.character.episode.map((episode) => {
-              return (
-                <li>
-                  <b>{episode.name}</b> - {episode.episode}
-                  <br></br>
-                  {episode.air_date}
-                </li>
-              );
-            })}
-          </ul>
-          <br></br>
-        </div>
-      </div>
-    </div>
+            <Row className="p-3">
+              <Button variant="primary" onClick={handleShow}>
+                Add Location
+              </Button>
+
+              <ModalComp show={show} onHide={handleClose} />
+            </Row>
+          </Col>
+          <Col sm md>
+            <h2>{data.character.name}</h2>
+            <div>
+              <b>Status:</b> {data.character.status}
+              <br></br>
+              <b>Species:</b> {data.character.species}
+              <br></br>
+              <b>Gender:</b> {data.character.gender}
+              <br></br>
+              <b>Origin:</b> {data.character.origin.name}
+              <br></br>
+              <b>Episodes:</b>
+              <ul>
+                {data.character.episode.map((episode) => {
+                  return (
+                    <li>
+                      <b><i>{episode.name}</i></b> - {episode.episode}
+                      <br></br>
+                      {episode.air_date}
+                    </li>
+                  );
+                })}
+              </ul>
+              <br></br>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
