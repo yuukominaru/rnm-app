975a9676dcb16304de860dfc048d54c342029847
yuukominaru <patriciaaela@gmail.com>
2023-07-31 01:02:11 +0700

    cleanup code

--- src/components/CardComp.js
+++ src/components/CardComp.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Col from 'react-bootstrap/Col';
+import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 
 export const CardComp = ({ character }) => {
